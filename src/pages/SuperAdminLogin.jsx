8028a5ff347ec2cc9f517a69b14ff54ffc7c96fc
Favour <georgechiamaka02@gmail.com>
2026-03-05 16:12:36 +0100

    fix: add vercel routing config

--- src/pages/SuperAdminLogin.jsx
+++ src/pages/SuperAdminLogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Shield, Mail, Lock, Eye, EyeOff, AlertCircle } from 'lucide-react';
-import { ButtonSpinner } from '../components/LoadingSpinner';
+import { ButtonSpinner } from '../Components/LoadingSpinner';
 
 
 
