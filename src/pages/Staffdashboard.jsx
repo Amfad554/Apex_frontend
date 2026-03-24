6b636ee936f449bfd104ab210eb25a18a6aef450
Favour <georgechiamaka02@gmail.com>
2026-03-24 15:35:34 +0100

    updated staff dashboard

--- src/pages/Staffdashboard.jsx
+++ src/pages/Staffdashboard.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
     Activity, AlertCircle, Bell, Calendar, ChevronDown,
-    ClipboardList, Clock, Eye, FileText, Heart,
+    ClipboardList, Clock, CreditCard, Eye, FileText, Heart,
     Home, LogOut, Menu, Moon, Pill, Lock,
     Search, Stethoscope, Sun,
     User, Users, X, Plus, CheckCircle,
