b3b97278e25394c7c8e19b33499ca2bf65ec5871
Favour <georgechiamaka02@gmail.com>
2026-03-30 09:49:20 +0100

    updated contact page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
-import ScrollToTop from "./ScrollToTop";
+import ScrollToTop from "../Components/ScrollToTop";
 
 export default function Layout() {
   return (
