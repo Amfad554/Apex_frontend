b483ac96217364cf6c1c5b8a9663a46ca445a664
Favour <georgechiamaka02@gmail.com>
2026-03-30 09:46:52 +0100

    updated contact page

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import ScrollToTop from "./ScrollToTop";
 
-// Public layout — used for landing, features, pricing, etc.
 export default function Layout() {
   return (
     <>
       <Navbar />
       <Outlet />
       <Footer />
+      <ScrollToTop />
     </>
   );
 }
